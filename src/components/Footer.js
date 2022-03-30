c7e79b863ff15db1a9c9414b05f04066eb34592e
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2022-03-30 01:39:25 +0100

    app complete-01

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <div className='footer'>
             <p className='footer__name'>© {new Date().getFullYear()} Airbnb clone! All rights reserved</p>
-            <a target="_blank" href='https://exclusiveideas-c9470.web.app/' className='footer__company'>Exclusive Ideas</a>
+            <a target="_blank" href='https://exclusiveideas-c9470.web.app' rel="noreferrer" className='footer__company'>Exclusive Ideas</a>
             <p className='footer__privacy'>Privacy · Terms · Sitemap · Company Details</p>
         </div>
     )
--- src/components/constants.js
+++ src/components/constants.js
@@ -78,8 +78,7 @@ export const searchResultInfo = [
         total: "£117 total",
     },
     {
-        // img:"https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+        img:"https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg",
         location: "Private room in center of London",
         title: "Independant luxury studio apartment",
         description: "2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen",
@@ -88,8 +87,7 @@ export const searchResultInfo = [
         total: "£157 total",
     },
     {
-        // img:"https://www.smartertravel.com/uploads/2017/07/Untitled-design-8.jpg",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+        img:"https://www.smartertravel.com/uploads/2017/07/Untitled-design-8.jpg",
         location: "Private room in center of London",
         title: "London Studio Apartments",
         description: "4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine",
@@ -98,8 +96,7 @@ export const searchResultInfo = [
         total: "£207 total"
     },
     {
-        // img:"https://cdn.bisnow.net/fit?height=489&type=jpeg&url=https%3A%2F%2Fs3.amazonaws.com%2Fcdn.bisnow.net%2Fcontent%2Fimages%2F2017%2F05%2F59151d0978bbf_https_press_atairbnb_com_app_uploads_2016_12_midtown_4.jpeg&width=717&sign=FeltIPi9cOWA36nVIeDvZxwgtiCZrpUyMRdvyZviTUI"
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+        img:"https://cdn.bisnow.net/fit?height=489&type=jpeg&url=https%3A%2F%2Fs3.amazonaws.com%2Fcdn.bisnow.net%2Fcontent%2Fimages%2F2017%2F05%2F59151d0978bbf_https_press_atairbnb_com_app_uploads_2016_12_midtown_4.jpeg&width=717&sign=FeltIPi9cOWA36nVIeDvZxwgtiCZrpUyMRdvyZviTUI",
         location: "Private room in center of London",
         title: "30 mins to Oxford Street, Excel London",
         description: "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
@@ -108,8 +105,7 @@ export const searchResultInfo = [
         total: "£320 total",
     },
     {
-        // img:"https://media.cntraveler.com/photos/5a8f258bd363c34048b35aac/master/w_2250,h_1500,c_limit/airbnb-plus-london.jpg",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+        img:"https://media.cntraveler.com/photos/5a8f258bd363c34048b35aac/master/w_2250,h_1500,c_limit/airbnb-plus-london.jpg",
         location: "Private room in center of London",
         title: "Spacious Peaceful Modern Bedroom",
         description: "3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Free parking · Dry Cleaning",
@@ -118,8 +114,7 @@ export const searchResultInfo = [
         total: "£450 total",
     },
     {
-        // img:"https://static.trip101.com/paragraph_media/pictures/001/676/061/large/969ae4bb-efd1-4fb9-a4e3-5cb3316dd3c9.jpg?1562227937",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+        img:"https://static.trip101.com/paragraph_media/pictures/001/676/061/large/969ae4bb-efd1-4fb9-a4e3-5cb3316dd3c9.jpg?1562227937",
         location: "Private room in center of London",
         title: "The Blue Room In London",
         description: "2 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Washing Machine",
@@ -128,8 +123,7 @@ export const searchResultInfo = [
         total: "£480 total",
     },
     {
-        // img:"https://image.insider.com/585029a0dd0895bc548b4b8b?width=750&format=jpeg&auto=webp",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+        img:"https://image.insider.com/585029a0dd0895bc548b4b8b?width=750&format=jpeg&auto=webp",
         location: "Private room in center of London",
         title: "5 Star Luxury Apartment",
         description: "3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
@@ -138,8 +132,7 @@ export const searchResultInfo = [
         total: "£650 total",
     },
     {
-        // img:"https://cdn.bisnow.net/fit?height=489&type=jpeg&url=https%3A%2F%2Fs3.amazonaws.com%2Fcdn.bisnow.net%2Fcontent%2Fimages%2F2017%2F05%2F59151d0978bbf_https_press_atairbnb_com_app_uploads_2016_12_midtown_4.jpeg&width=717&sign=FeltIPi9cOWA36nVIeDvZxwgtiCZrpUyMRdvyZviTUI"
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+        img:"https://cdn.bisnow.net/fit?height=489&type=jpeg&url=https%3A%2F%2Fs3.amazonaws.com%2Fcdn.bisnow.net%2Fcontent%2Fimages%2F2017%2F05%2F59151d0978bbf_https_press_atairbnb_com_app_uploads_2016_12_midtown_4.jpeg&width=717&sign=FeltIPi9cOWA36nVIeDvZxwgtiCZrpUyMRdvyZviTUI",
         location: "Private room in center of London",
         title: "30 mins to Oxford Street, Excel London",
         description: "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
@@ -148,8 +141,7 @@ export const searchResultInfo = [
         total: "£320 total",
     },
     {
-        // img:"https://media.cntraveler.com/photos/5a8f258bd363c34048b35aac/master/w_2250,h_1500,c_limit/airbnb-plus-london.jpg",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+        img:"https://media.cntraveler.com/photos/5a8f258bd363c34048b35aac/master/w_2250,h_1500,c_limit/airbnb-plus-london.jpg",
         location: "Private room in center of London",
         title: "Spacious Peaceful Modern Bedroom",
         description: "3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Free parking · Dry Cleaning",
@@ -158,8 +150,7 @@ export const searchResultInfo = [
         total: "£450 total",
     },
     {
-        // img:"https://static.trip101.com/paragraph_media/pictures/001/676/061/large/969ae4bb-efd1-4fb9-a4e3-5cb3316dd3c9.jpg?1562227937",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+        img:"https://static.trip101.com/paragraph_media/pictures/001/676/061/large/969ae4bb-efd1-4fb9-a4e3-5cb3316dd3c9.jpg?1562227937",
         location: "Private room in center of London",
         title: "The Blue Room In London",
         description: "2 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Washing Machine",
@@ -168,8 +159,7 @@ export const searchResultInfo = [
         total: "£480 total",
     },
     {
-        // img:"https://image.insider.com/585029a0dd0895bc548b4b8b?width=750&format=jpeg&auto=webp",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+        img:"https://image.insider.com/585029a0dd0895bc548b4b8b?width=750&format=jpeg&auto=webp",
         location: "Private room in center of London",
         title: "5 Star Luxury Apartment",
         description: "3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
